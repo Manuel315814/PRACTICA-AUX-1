948c257774b1d76d7421dd227d0ba25f8821cfcb
Carmanu <manuelbaltazar008@gmail.com>
2025-03-08 21:03:06 -0400

    Proyec 2

--- ejercicio.py
+++ ejercicio.py
@@ -5,5 +5,13 @@ num2 = float(input("INGRESE UN NUMERO: "))
 s = num1 + num2
 r = num1 - num2
 mult = num1 * num2
-
+div = num1 / num2
+print ("LA SUMA ES: ")
+print (s)
+print ("LA RESTA ES: ")
+print (r)
+print ("LA MULTIPLICACION ES: ")
+print (mult)
+print ("LA DIVISION ES: ")
+print (div)
 
