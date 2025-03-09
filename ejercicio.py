7b8ba87c1405095a3b4c8e14237d91d5fb62faa6
Carmanu <manuelbaltazar008@gmail.com>
2025-03-08 20:49:44 -0400

    Proyec 1

---
+++ ejercicio.py
@@ -0,0 +1,9 @@
+# SOLICITAR LOS NUMEROS AL USUARIO
+num1 = float(input("INGRESAR UN NUMERO: "))
+num2 = float(input("INGRESE UN NUMERO: "))
+# REALIZAMOS LAS OPERACIONES///
+s = num1 + num2
+r = num1 - num2
+mult = num1 * num2
+
+
